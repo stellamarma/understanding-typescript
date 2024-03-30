568f1464686d0cfce960998de876a31222fbb825
stellamarma <stellamarmat@gmail.com>
2024-03-30 14:31:15 +0100

    lesson30

--- using-ts.ts
+++ using-ts.ts
@@ -40,3 +40,9 @@ userInput='max';
 if(typeof userInput ==='string'){
     userName=userInput;
 }
+function generateError(message:string, code:number):never{
+
+    throw{message: message,errorCode:code};
+}
+const result=generateError('An error occurred',500);
+console.log(result);
