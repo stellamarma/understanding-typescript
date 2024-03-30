513883a23779cf1efc679fd765a1f2b1e1e92142
stellamarma <stellamarmat@gmail.com>
2024-03-30 07:47:05 +0100

    lesson19

--- using-ts.ts
+++ using-ts.ts
@@ -1,12 +1,18 @@
 
-const person ={
+const person:{
+    name: string;
+    age:number;
+    hobbies:string[];
+    role:[number,string];
+} ={
     name: 'maxmal',
     age: 30,
     hobbies:[ 'Sport','Cooking']
-
+    role: [2,'author']
 
 };
-
+person.role.push('admin');
+//person.role[1]=10; its wrong
 let favoriteActivities: string[];
 favoriteActivities= ['Sport'];
 
