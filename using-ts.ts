6c017febe39e0772c19440d59451135c5a06af77
stellamarma <stellamarmat@gmail.com>
2024-03-30 08:09:37 +0100

    lesson20

--- using-ts.ts
+++ using-ts.ts
@@ -1,14 +1,15 @@
 
-const person:{
-    name: string;
-    age:number;
-    hobbies:string[];
-    role:[number,string];
-} ={
+/*const ADMIN=0;
+const READ_ONLY=1;
+const AUTHOR=2;
+its the same as line 7
+*/
+enum Role{ADMIN,READ_ONLY, AUTHOR}
+const person ={
     name: 'maxmal',
     age: 30,
     hobbies:[ 'Sport','Cooking']
-    role: [2,'author']
+    role: Role.ADMIN // i have access to the value
 
 };
 person.role.push('admin');
@@ -17,6 +18,3 @@ let favoriteActivities: string[];
 favoriteActivities= ['Sport'];
 
 console.log(person.name);
-for(const hoddy of person.hobbies){
-    console.log(hobby.toUpperCase());
-}
