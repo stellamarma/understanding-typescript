f48c8e49d3dabcdd523448ed5623112abe59a9a1
stellamarma <stellamarmat@gmail.com>
2024-03-31 08:52:15 +0200

    lesson36

--- using-ts.js
+++ using-ts.js
@@ -1,7 +1,2 @@
-function add(num1, num2) {
-    return num1 + num2;
-}
-var number1 = 5;
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+"use strict";
+console.log('Sending..');
