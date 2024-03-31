2b75d7668a9ad7902035184ab700ea93f8e5b24d
stellamarma <stellamarmat@gmail.com>
2024-03-31 14:53:57 +0200

    lesson50

--- using-ts.ts
+++ using-ts.ts
@@ -11,3 +11,8 @@ if (age>20){
 }
 
 console.log(isOld);
+const add =(a:number,b:number)=>a+b;
+console.log(add(2,5));
+
+const printOutput=(output:string|number)=>console.log(output);
+printOutput(add(2,5));
