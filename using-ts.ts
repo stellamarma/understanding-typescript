a73b0b12e05f2aa214549e142bc42de8037c4ef2
stellamarma <stellamarmat@gmail.com>
2024-04-01 11:38:43 +0200

    lesson59: Creating a first class

--- using-ts.ts
+++ using-ts.ts
@@ -1,44 +1,9 @@
-
-let age;
-
-const userName='Max';
-
-age=30;
-console.log(userName);
-
-if (age>20){
-    var isOld=true;
+class Department{
+    name: string;
+    construction(n:string){
+        this.name=n;
+    }
 }
+const accounting = new Department('Accounting');
 
-
-const add =(...numbers:number[])=>{
-    return numbers.reduce((curResult,curValue)=>{
-        return curResult+curValue;
-    },0);
-   
-};
-const addNumbers=add(2,5,2,3);
-console.log(addNumbers);
-
-const printOutput=(output:string|number)=>console.log(output);
-printOutput(add(2));
-
-const hobbies =[ 'sport', 'cooking'];
-const activeHobbies=['hiking'];
-
-activeHobbies.push(...hobbies);
-
-const person={
-    name:'Max',
-    age:30
-};
-
-const copiedPerson={...person};
-
-const hoddy1= hobbies[0];
-const hoddy2= hobbies[1];
-const [hpbby1, hobby2,...remain ]=hobbies;
-console.log(hobbies)
-
-const { name:firstname , age }= person;
-console.log(firstname , age);
+console.log(accounting)
