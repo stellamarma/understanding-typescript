65db047906478369252edd2d7805e6c24b638fb1
stellamarma <stellamarmat@gmail.com>
2024-04-03 10:51:31 +0300

    lesson62: private & public

--- using-ts.ts
+++ using-ts.ts
@@ -1,9 +1,26 @@
 class Department{
     name: string;
-    construction(n:string){
+    private employees:string[]=[];
+    constructor(n:string){
         this.name=n;
+    }
+    describe(this: Department){
+        console.log('Deparment:'+ this.name);
+    }
+    addEmployee(employee:string){
+        this.employees.push(employee);
+    }
+    printEmployeeInformation(){
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 const accounting = new Department('Accounting');
 
-console.log(accounting)
+
+
+console.log(accounting);
+
+const accountntingCopy= {name:'s',describe: accounting.describe};
+
+accountntingCopy.describe();
