070c705a371298c2528ed327f4e8aae266535bf7
stellamarma <stellamarmat@gmail.com>
2024-04-04 12:16:52 +0300

    lesson85: Discriminated Unions

--- using-ts.ts
+++ using-ts.ts
@@ -1,6 +1,6 @@
 type Admin{
     name: string;
-    privileges: string;
+    privileges: string[];
 };
 type Employee{
     name:string;
@@ -11,7 +11,31 @@ type ElevatedEmployee= Admin & Employee;
 
 const e1: ElevatedEmployee={
     name: 'Max',
-    privileges: 'create-server',
+    privileges: ['create-server'],
     startDate:new Date()
 
 };
+type Combinable= string|number;
+type Numeric= number | boolean;
+
+type Universal= Combinable & Numeric;
+
+function add(a:Combinable, b: Numeric){
+    if (typeof a==='string'|| typeof b === 'string'){
+        return a.toString()+ b.toString();
+    }
+    return a+b;
+}
+ type UnknownEmployee= Employee|Admin ;
+
+ function printEmployeeInformation(emp: UnknownEmployee){
+    console.log('Name:' + emp.name);
+
+    if('privaleges' in emp)
+        console.log('Privileges: ' +emp.startDate);//typeGuard
+
+ }
+
+ printEmployeeInformation(e1);
+
+ class Car  
