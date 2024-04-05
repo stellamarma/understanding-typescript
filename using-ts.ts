9316b21ed0073cc65c5f5fa6f3b0cbbec7bdfe3d
stellamarma <stellamarmat@gmail.com>
2024-04-05 11:40:00 +0300

    lesson95: Creating a Generic Function

--- using-ts.ts
+++ using-ts.ts
@@ -1,3 +1,4 @@
+/*
 const names: Array<string>=[];
 
 const promise: Promise<string>=new Promise((resolve, reject))=>{
@@ -9,3 +10,10 @@ const promise: Promise<string>=new Promise((resolve, reject))=>{
 promise.then(data=>{
     data.split('');
 })
+*/
+function merge<T,U>(objA: T,objB: U){
+    return Object.assign(objA,objB);
+}
+const mergedObj= merge({name:'max'},{age:30});
+console.log(mergedObj);
+
