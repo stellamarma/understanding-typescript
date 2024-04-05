24f4f4b9ef2caa837113108719c3347561d3ed4d
stellamarma <stellamarmat@gmail.com>
2024-04-05 14:44:25 +0300

    lesson96: Working with Constraints

--- using-ts.ts
+++ using-ts.ts
@@ -11,9 +11,10 @@ promise.then(data=>{
     data.split('');
 })
 */
-function merge<T,U>(objA: T,objB: U){
+function merge<T extends object,U extends object>(objA: T,objB: U){
     return Object.assign(objA,objB);
 }
 const mergedObj= merge({name:'max'},{age:30});
 console.log(mergedObj);
 
+ 
