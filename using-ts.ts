341585d14cd16ff234fc7bd6f1b527f6d51bb962
stellamarma <stellamarmat@gmail.com>
2024-04-06 14:01:55 +0300

    lesson101: Generic Utility Types

--- using-ts.ts
+++ using-ts.ts
@@ -68,3 +68,23 @@ interface lengthy{
  objStorage.addItem({name:'max'});
  objStorage.addItem({name:'manu'});
  */ 
+interface CourseGoal{
+
+    title:string;
+    description:string;
+    completeUntil:Date;
+}
+
+function createCourseGoal(
+    title:string,
+    description:string,
+    date:Date)
+     :CourseGoal{
+    let courseGoal:Partial<CourseGoal>={};
+    courseGoal.title=title;
+    courseGoal.description=description;
+    courseGoal. completeUntil=date;
+    return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]>=['max','anna'];
